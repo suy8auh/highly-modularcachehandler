ae679f11126d550571f2208231703502646ff582
suy8auh <gs1djdndjdi@hotmail.com>
2024-05-10 01:25:45 +0700

    hello

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const countingSort = (arr) => {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-  for (let num of arr) {
-    count[num - min]++;
-  }
-  let sortedIndex = 0;
-  for (let i = min; i <= max; i++) {
-    while (count[i - min] > 0) {
-      arr[sortedIndex++] = i;
-      count[i - min]--;
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
     }
   }
-  return arr;
+  return -1;
 };
