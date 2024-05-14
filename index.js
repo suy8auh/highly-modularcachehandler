66dbca7f954a70ccdee73acaa5e233625e0f48c0
suy8auh <gs1djdndjdi@hotmail.com>
2024-05-15 03:44:50 +0700

    test commit

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-      else left = mid + 1;
-    } else {
-      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-      else right = mid - 1;
-    }
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) {
+    result ^= num;
   }
-  return -1;
+  return result;
 }
